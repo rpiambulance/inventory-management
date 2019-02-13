d8e99a521c3eb54930ba81ae9a3541b63ac298c6
Andrew Qu <qua@rpi.edu>
2019-02-13 14:51:05 -0500

    initial inventory planning

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
