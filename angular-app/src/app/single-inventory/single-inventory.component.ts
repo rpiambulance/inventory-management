950da89889dae274e3f11f6837e81f9084ed790f
Andrew Qu <qua@rpi.edu>
2019-02-26 18:10:51 -0500

    started on single

--- angular-app/src/app/single-inventory/single-inventory.component.ts
+++ angular-app/src/app/single-inventory/single-inventory.component.ts
@@ -11,7 +11,7 @@ import { User } from '../user';
 })
 export class SingleInventoryComponent implements OnInit {
 
-  @Input() user: User;
+  @Input() user: string;
   constructor() { }
 
   ngOnInit() {
