9d13c4c307862d7ce28026accec9eaea7262178a
Andrew Qu <qua@rpi.edu>
2019-02-26 21:52:47 -0500

    finished single inventory display successfully displays each item and quantity based on the json returned edited mock data to make it a little more different between each entry

--- angular-app/src/app/single-inventory/single-inventory.component.ts
+++ angular-app/src/app/single-inventory/single-inventory.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Inventory } from '../inventory';
+import { InventoryComponent } from '../inventory/inventory.component';
 import { currentUser, MOCK_USERS } from '../mock-data';
 import { GetInventoriesService } from '../get-inventories.service';
 import { User } from '../user';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-inventory',
@@ -11,10 +13,25 @@ import { User } from '../user';
 })
 export class SingleInventoryComponent implements OnInit {
 
+  inventoriesJSON: Inventory[];
+  inv: string;
+  thisInv: Inventory;
   @Input() user: string;
-  constructor() { }
+  constructor(private route: ActivatedRoute, private data: GetInventoriesService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.inv = params.get('name');
+    });
+    // get the data from the server
+    this.data.getInventories().subscribe(data => {
+      this.inventoriesJSON = data;
+      for (const inv of this.inventoriesJSON) {
+        if (inv.name === this.inv) {
+          this.thisInv = new Inventory(inv);
+        }
+      }
+    });
   }
 
 }
