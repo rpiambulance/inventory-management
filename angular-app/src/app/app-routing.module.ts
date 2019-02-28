b56cf5f03ca65917e9d01071c2270e38bb1f364a
JellyWang7 <30369722+JellyWang7@users.noreply.github.com>
2019-02-28 15:28:39 -0500

    styled landing page and added images

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { SingleInventoryComponent } from './single-inventory/single-inventory.component';
 import { NewInvFormComponent } from './new-inv-form/new-inv-form.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { LandingPageComponent}  rom './landing-page/landing-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'inventory', component: InventoryComponent },
   { path: 'single-inventory/:name', component: SingleInventoryComponent },
   { path: 'register', component: RegisterComponent},
-  { path: 'newInvForm', component: NewInvFormComponent}
+  { path: 'newInvForm', component: NewInvFormComponent},
+  { path: 'addItemForm', component: AddItemFormComponent},
+  { path: 'landing-page',component: LandingPageComponent}
 ];
 
 @NgModule({
