89e50d36feab349a067547fbf10d3b76b05afdb4
Logan Ramos <lramos15@gmail.com>
2019-03-12 11:06:41 -0400

    Fixed some error messages and added logout functionality

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router, private authServ: AuthService) {}
   title = 'Inventory Meme System';
+  loggedIn = localStorage.getItem('auth_token') != null;
+
+  logoutUser():void {
+    this.authServ.logout();
+    this.router.navigate(['login']);
+  }
+
+  changeOfRoutes():void {
+    // we must check on every route change for the navbar to update
+    this.loggedIn = localStorage.getItem('auth_token') != null;
+  }
 }
