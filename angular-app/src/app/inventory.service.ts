0aedd96763050a826412bff1eb7c56dbdc7f5b9f
Logan Ramos <lramos15@gmail.com>
2019-03-12 09:59:18 -0400

    Initial backend (#22)
    * Initial backend work
    
    * More backend work
    
    * You can now add inventory and add items, not completely tied into front end
    
    * added mock-data for demo
    
    * User registration WIP
    
    * Login now works
    
    * Replaced all mock data with inventory backend
    
    * Fixed navigation

---
+++ angular-app/src/app/inventory.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
+import { Item } from './item';
+import { Inventory } from './inventory';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+
+  constructor(private http: HttpClient) { }
+
+  getInventories(): Observable<Inventory[]> {
+
+    return this.http.get<any>('http://localhost:3000/inventory').pipe(map(res => {
+      const inventories = [];
+      res = res.inventories;
+      for (let i = 0; i < res.length; i++) {
+        inventories[i] = new Inventory(res[i]);
+      }
+      return inventories;
+    }));
+  }
+
+  createInventory(inv: Inventory): Observable<any> {
+    return this.http.post('http://localhost:3000/inventory/create', inv);
+  }
+
+  addItem(inv: Inventory, itemsArray: Item[]): Observable<Inventory> {
+    return this.http.post<Inventory>('http://localhost:3000/inventory/additem', {inventory: inv, items: itemsArray});
+  }
+}
--- angular-app/src/app/login/login.component.ts
+++ angular-app/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from '../user';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +10,25 @@ import { User } from '../user';
 })
 export class LoginComponent {
 
-  constructor() { }
+  signinError: string;
+  signinSuccess: boolean;
+  constructor(private authServ: AuthService, public router: Router) {
+    this.signinSuccess = true;
+  }
 
   model: User = new User('', '', '', '', '');
   onSubmit(): void {
     // We will do the actual submission of the form here
     console.log('Submitted!');
+    this.authServ.authenticateUser(this.model).subscribe((response) => {
+      this.signinError = response.error;
+      this.signinSuccess = response.success;
+      if (response.success) {
+        // Signed in successfully we add the response token
+        localStorage.setItem('auth_token', response.token);
+        this.router.navigate(['inventory']);
+      }
+    });
     return null;
   }
 
